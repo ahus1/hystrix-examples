155afc7979377106522c3d4e07f2cc2cdd9479f7
Alexander Schwartz <alexander.schwartz@gmx.net>
2014-09-01 18:48:30 +0200

    building documentation with screen shot for zabbix

--- manual/build.bat
+++ manual/build.bat
@@ -2,5 +2,6 @@ mkdir ..\..\hystrix-pages\manual
 copy ..\tools\tomcat\manual\*.png ..\..\hystrix-pages\manual
 copy ..\tools\jmeter\manual\*.png ..\..\hystrix-pages\manual
 copy ..\tools\riemann\manual\*.png ..\..\hystrix-pages\manual
+copy ..\tools\zabbix\manual\*.png ..\..\hystrix-pages\manual
 copy manual\*.png ..\..\hystrix-pages\manual
 asciidoctor manual.adoc -d book -D ..\..\hystrix-pages
