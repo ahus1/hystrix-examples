398f3dffb3993d125baf2b3fcbcc3cb2dd9624c2
Alexander Schwartz <alexander.schwartz@gmx.net>
2014-09-22 10:24:14 +0200

    fix folder

--- manual/build.sh
+++ manual/build.sh
@@ -1,7 +1,9 @@
 mkdir -p $HOME/pages/manual
-copy ../tools/tomcat/manual/*.png $HOME/pages/manual
-copy ../tools/jmeter/manual/*.png $HOME/pages/manual
-copy ../tools/riemann/manual/*.png $HOME/pages/manual
-copy ../tools/zabbix/manual/*.png $HOME/pages/manual
-copy manual/*.png $HOME/pages/manual
-asciidoctor manual.adoc -d book -D$HOME/pages
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+cp ../tools/tomcat/manual/*.png $HOME/pages/manual
+cp ../tools/jmeter/manual/*.png $HOME/pages/manual
+cp ../tools/riemann/manual/*.png $HOME/pages/manual
+cp ../tools/zabbix/manual/*.png $HOME/pages/manual
+cp manual/*.png $HOME/pages/manual
+asciidoctor manual.adoc -d book -D $HOME/pages
